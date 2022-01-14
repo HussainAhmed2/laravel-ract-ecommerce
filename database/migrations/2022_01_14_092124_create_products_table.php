a4840f754bae43f54545f6e2d0087f7da49eefee
hussain <dev.ha@officesquare.co>
2022-01-14 15:08:58 +0500

    product migrations added

--- database/migrations/2022_01_14_092124_create_products_table.php
+++ database/migrations/2022_01_14_092124_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCoursesTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,13 @@ class CreateCoursesTable extends Migration
      */
     public function up()
     {
-        Schema::create('courses', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
+            $table->string('image');
+            $table->string('product_name');
+            $table->integer('product_price');
+            $table->integer('product_qty');
+            $table->text('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -28,6 +32,6 @@ class CreateCoursesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('courses');
+        Schema::dropIfExists('products');
     }
 }
